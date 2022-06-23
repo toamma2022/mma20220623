c598dfee7710bcfc8b072cf4357b199aa6799178
unvsacrs <toa16659_n0zak1@au.com>
2022-06-23 10:24:45 +0900

    Initial Commit

---
+++ mma20220623/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mma20220623
+//
+//  Created by YASUSHI NOZAKI on R 4/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mma20220623/mma20220623App.swift
@@ -0,0 +1,17 @@
+//
+//  mma20220623App.swift
+//  mma20220623
+//
+//  Created by YASUSHI NOZAKI on R 4/06/23.
+//
+
+import SwiftUI
+
+@main
+struct mma20220623App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
