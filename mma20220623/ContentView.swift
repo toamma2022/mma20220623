d415d47fe67e838a8bdf996aa8ef2d7b7a5c2e5e
unvsacrs <toa16659_n0zak1@au.com>
2022-06-30 11:16:04 +0900

    傾きセンサーに合わせて矢印の向きを変更する

--- mma20220623/ContentView.swift
+++ mma20220623/ContentView.swift
@@ -6,20 +6,48 @@
 //
 
 import SwiftUI
+import CoreMotion
+
 
 struct ContentView: View {
-    @State private var direction = 0.0
-
+    @State private var angle = 0.5
+    
+    // MotionManager
+    let motionManager = CMMotionManager()
+    
+    
+    
     var body: some View {
-        Image("leftarrow").rotationEffect(.degrees(Double(self.direction * 180)))
-            .padding()
-        Slider(value: $direction,
-               in: 0...1,
-               onEditingChanged: { _ in
-            })
+        VStack {
+            Image("leftarrow").rotationEffect(.degrees(Double(self.angle)))
+                .padding()
+        }
+        .onAppear() {
+            if self.motionManager.isAccelerometerAvailable {
+                // intervalの設定 [sec]
+                self.motionManager.accelerometerUpdateInterval = 0.05
+
+                // センサー値の取得開始
+                self.motionManager.startAccelerometerUpdates(
+                    to: OperationQueue.current!,
+                    withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
+                        if let accelData = accelData {
+                            self.outputAccelData(acceleration: accelData.acceleration)
+                        }
+                    })
+        }
+            
+        }
+
+    }
+
+    func outputAccelData(acceleration: CMAcceleration){
+        // 加速度センサー [G]
+        print(String(format: "x:%06f, y:%06f, z:%06f", acceleration.x, acceleration.y, acceleration.z))
+        self.angle = -90 * acceleration.x + 90
     }
 }
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
